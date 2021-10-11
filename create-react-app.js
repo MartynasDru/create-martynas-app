84429df02f292beae61e3258bfa4b6cb348748ba
Martynas Druteika <martynasdd@gmail.com>
2021-10-11 20:22:57 +0300

    Implemented package.json setup

--- create-react-app.js
+++ create-react-app.js
@@ -1,4 +1,5 @@
 const fse = require("fs-extra");
+const fs = require("fs");
 const path = require("path");
 const chalk = require("chalk")
 
@@ -7,29 +8,40 @@ const projectName = args[0];
 const projectDir = path.resolve(`./${projectName}`);
 const templatePath = __dirname + "/template";
 
-function init() {
+const init = () => {
   if (args.length === 0) {
-    console.log();
-    console.log("Please specify the project directory:")
-    console.log(
-      `    ${chalk.cyan("@martynasdd/create-martynas-app")} ${chalk.green("<project-directory>")}`
-    );
-    console.log();
-    console.log("For example:")
-    console.log(
-      `    ${chalk.cyan("@martynasdd/create-martynas-app")} ${chalk.green("my-react-app")}`
-    );
-    console.log();
+    noArgsGuidelines();
   } else {
     copyTemplate();
+    setupPackageJSON();
+
+    console.log(chalk.green(`Project ${projectName} setup finished!`));
   }
 }
 
 function copyTemplate() {
-  fse.copy(templatePath, projectDir, err => {
-    if (err) return console.error(err);
-    console.log("success!");
-  })
+  fse.copySync(templatePath, projectDir);
+}
+
+function noArgsGuidelines() {
+  console.log();
+  console.log("Please specify the project directory:")
+  console.log(
+    `    ${chalk.cyan("@martynasdd/create-martynas-app")} ${chalk.green("<project-directory>")}`
+  );
+  console.log();
+  console.log("For example:")
+  console.log(
+    `    ${chalk.cyan("@martynasdd/create-martynas-app")} ${chalk.green("my-react-app")}`
+  );
+  console.log();
+}
+
+function setupPackageJSON() {
+  const packageJSON = fse.readJsonSync(`${projectDir}/package.json`);
+  packageJSON.name = projectName;
+
+  fs.writeFileSync(`${projectDir}/package.json`, JSON.stringify(packageJSON, null, 2));
 }
 
 module.exports = {
